fe1a0889c2a1328c1f21f7ff1756045dc73a71d2
吳元皓 <hpware3@gmail.com>
2024-07-04 00:11:08 +0800

    redo the images!

--- posts/2.vue
+++ posts/2.vue
@@ -4,10 +4,10 @@
         <h8>2024/6/20</h8>
         <p>在網站放上Vercel時，就有美國(? 的人看??????? 所以我就因為這樣安裝了Google的追蹤器</p>
         <p>Here's some result （來自 Vercel)</p>
-        <img src="https://storage.hpware.hwtw.cc/img/#2/vercel-data.png">
+        <img src="https://wepr3os.fastmaildeliverycontent.hwtw.cc/blogpost-2/vercel-data.png">
         <p>Google (如果有用uBlock Orgin 或 AdBlock 的話應該不會看到)</p>
-        <img src="https://storage.hpware.hwtw.cc/img/#2/google-data-1.png">
-        <img src="https://storage.hpware.hwtw.cc/img/#2/google-data-2.png">
+        <img src="https://wepr3os.fastmaildeliverycontent.hwtw.cc/blogpost-2/google-data-1.png">
+        <img src="https://wepr3os.fastmaildeliverycontent.hwtw.cc/blogpost-2/google-data-2.png">
         <p>NOTE: 這個一定不是 Search Engine 的 Trafic</p>
     </div>
 </template>
